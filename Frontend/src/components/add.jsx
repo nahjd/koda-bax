b1f9c047684ea616e3971ff95e8b40d0d63b3b46
nahjd <nahidmemmedov711@gmail.com>
2024-01-19 07:40:24 +0400

    asdasd

--- Frontend/src/components/add.jsx
+++ Frontend/src/components/add.jsx
@@ -1,9 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import { fetchData, postData } from '../redux/slice/userSlice';
+import { deletedData, fetchData, postData } from '../redux/slice/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const SignupSchema = Yup.object().shape({
     firstName: Yup.string()
@@ -18,6 +26,8 @@ const SignupSchema = Yup.object().shape({
 });
 
 const add = () => {
+    const [search, setSearch] = useState("")
+    const [type, settype] = useState("")
     const data = useSelector((state) => state.counter.data)
     const dispatch = useDispatch()
     useEffect(() => {
@@ -25,6 +35,19 @@ const add = () => {
     }, [dispatch])
 
     console.log(data);
+
+    const searchData = () => {
+        if (type == "az") {
+            return [...data].sort((a, b) => a.name.localCompare(b.name))
+
+        } else if (type = "za") {
+            return [...data.sort].sort((a, b) => a.name.localCompare(a.name))
+        } else {
+            return [...data].sort((a, b) => a.price - b.price)
+        }
+        return data
+
+    }
 
     return (
         <>
@@ -54,6 +77,83 @@ const add = () => {
                     </Form>
                 )}
             </Formik>
+            <div className="searc">
+                <TextField onChange={(e) => {
+                    setSearch(e.target.value)
+                }} placeholder='search' id="outlined-basic" label="Outlined" variant="outlined" />
+                <Button onClick={() => {
+                    settype("az")
+                }} type="submit" variant="contained" >A-Z</Button>
+                <Button onClick={() => {
+                    settype("za")
+                }} type="submit" variant="contained" >Z-A</Button>
+                <Button onClick={() => {
+                    settype("")
+                }} type="submit" variant="contained" >Price</Button>
+            </div>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>name</TableCell>
+                            <TableCell align="right">Calories</TableCell>
+                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {/* <TableBody>
+                        {searchData().filter((item) => {
+                            item.name.toLowerCase().includes(search.toLowerCase())
+                        }).map((row) => (
+                            <TableRow
+                                key={row._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                              
+                                <TableCell align="right">{row.image}</TableCell>
+
+                                <TableCell align="right">
+                                    <Button onClick={() => {
+                                        dispatch(deletedData(row._id))
+                                    }} variant="outlined" color="error" >Delete</Button>
+                                </TableCell>
+                            </TableRow>
+
+
+
+                        ))}
+                    </TableBody> */}
+
+                    <TableBody>
+                        {data && data.map((row) => (
+                            <TableRow
+                                key={row._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+
+                                <TableCell align="right">{row.image}</TableCell>
+
+                                <TableCell align="right">
+                                    <Button onClick={() => {
+                                        dispatch(deletedData(row._id))
+                                    }} variant="outlined" color="error" >Delete</Button>
+                                </TableCell>
+                            </TableRow>
+
+
+
+                        ))}
+                    </TableBody>
+
+                </Table>
+            </TableContainer>
         </>
     )
 }
